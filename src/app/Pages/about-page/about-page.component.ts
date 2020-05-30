b4bc5ac3eb25d6ac1ac6226238d482d2db5b8352
Tim Beswick <contact.tim.here@gmail.com>
2020-05-30 01:20:47 +0100

    Fix bad path for about page style

--- src/app/Pages/about-page/about-page.component.ts
+++ src/app/Pages/about-page/about-page.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
     selector: 'app-about-page',
     templateUrl: './about-page.component.html',
-    styleUrls: ['./about-page.component.scss', '/about-page.component.scss-theme.scss']
+    styleUrls: ['./about-page.component.scss', './about-page.component.scss-theme.scss']
 })
 export class AboutPageComponent {
     @Input() showRoute = true;
