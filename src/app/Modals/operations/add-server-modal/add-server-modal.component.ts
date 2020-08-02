c5edac99f9801aebb00128b11c30b2f4329c9ac8
Tim Beswick <contact.tim.here@gmail.com>
2020-08-02 16:20:44 +0100

    Fix bad variable reference

--- src/app/Modals/operations/add-server-modal/add-server-modal.component.ts
+++ src/app/Modals/operations/add-server-modal/add-server-modal.component.ts
@@ -10,52 +10,52 @@ import { MatDialog, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 @Component({
     selector: 'app-add-server-modal',
     templateUrl: './add-server-modal.component.html',
-    styleUrls: ['./add-server-modal.component.css']
+    styleUrls: ['./add-server-modal.component.css'],
 })
 export class AddServerModalComponent {
     form: FormGroup;
     instantErrorStateMatcher = new InstantErrorStateMatcher();
-    environment = [
+    environments = [
         { value: 0, viewValue: 'Release' },
         { value: 1, viewValue: 'Rc' },
-        { value: 2, viewValue: 'Dev' }
+        { value: 2, viewValue: 'Dev' },
     ];
     serverOptions = [
         { value: 0, viewValue: 'None' },
         { value: 1, viewValue: 'Run alone' },
-        { value: 2, viewValue: 'DCG' }
+        { value: 2, viewValue: 'DCG' },
     ];
     validationMessages = {
-        'name': [
+        name: [
             { type: 'required', message: 'Name is required' },
-            { type: 'serverTaken', message: 'That name is already in use' }
-        ], 'apiPort': [
+            { type: 'serverTaken', message: 'That name is already in use' },
+        ],
+        apiPort: [
             { type: 'required', message: 'Api Port is required' },
-            { type: 'serverTaken', message: 'That api port is already in use' }
-        ], 'port': [
-            { type: 'required', message: 'Port is required' }
-        ], 'numberHeadlessClients': [
-            { type: 'required', message: 'Headless client count is required' }
-        ], 'profileName': [
-            { type: 'required', message: 'Profile name is required' }
-        ], 'hostName': [
-            { type: 'required', message: 'Server display name is required' }
-        ], 'password': [
-            { type: 'required', message: 'Server password is required' }
-        ], 'adminPassword': [
-            { type: 'required', message: 'Admin password is required' }
-        ], 'environment': [
-            { type: 'required', message: 'Environment is required' }
-        ], 'serverOption': [
-            { type: 'required', message: 'Server option is required' }
-        ]
+            { type: 'serverTaken', message: 'That api port is already in use' },
+        ],
+        port: [{ type: 'required', message: 'Port is required' }],
+        numberHeadlessClients: [{ type: 'required', message: 'Headless client count is required' }],
+        profileName: [{ type: 'required', message: 'Profile name is required' }],
+        hostName: [{ type: 'required', message: 'Server display name is required' }],
+        password: [{ type: 'required', message: 'Server password is required' }],
+        adminPassword: [{ type: 'required', message: 'Admin password is required' }],
+        environment: [{ type: 'required', message: 'Environment is required' }],
+        serverOption: [{ type: 'required', message: 'Server option is required' }],
     };
     edit = false;
     changes = new Set<string>();
     server;
     submitting = false;
 
-    constructor(formbuilder: FormBuilder, private dialog: MatDialog, private dialogRef: MatDialogRef<AddServerModalComponent>, private httpClient: HttpClient, private urls: UrlService, @Inject(MAT_DIALOG_DATA) public data: any) {
+    constructor(
+        formbuilder: FormBuilder,
+        private dialog: MatDialog,
+        private dialogRef: MatDialogRef<AddServerModalComponent>,
+        private httpClient: HttpClient,
+        private urls: UrlService,
+        @Inject(MAT_DIALOG_DATA) public data: any
+    ) {
         this.form = formbuilder.group({
             name: ['', Validators.required, this.validateServer.bind(this)],
             port: ['', Validators.required],
@@ -66,14 +66,14 @@ export class AddServerModalComponent {
             password: ['', Validators.required],
             adminPassword: ['', Validators.required],
             environment: [0, Validators.required],
-            serverOption: [0, Validators.required]
+            serverOption: [0, Validators.required],
         });
         if (data) {
             this.edit = true;
             this.server = data.server;
             this.form.patchValue(this.server);
-            Object.keys(this.form.controls).forEach(key => {
-                this.form.get(key).valueChanges.subscribe(change => {
+            Object.keys(this.form.controls).forEach((key) => {
+                this.form.get(key).valueChanges.subscribe((change) => {
                     if (change !== this.server[key]) {
                         this.changes.add(key);
                     } else {
@@ -90,19 +90,19 @@ export class AddServerModalComponent {
         if (this.edit) {
             return timer(200).pipe(
                 switchMap(() => {
-                    if (control.pristine || control.value === null) { return of(null); }
-                    return this.httpClient.post(`${this.urls.apiUrl}/gameservers/${control.value}`, this.server).pipe(
-                        map(response => (response ? { serverTaken: true } : null))
-                    );
+                    if (control.pristine || control.value === null) {
+                        return of(null);
+                    }
+                    return this.httpClient.post(`${this.urls.apiUrl}/gameservers/${control.value}`, this.server).pipe(map((response) => (response ? { serverTaken: true } : null)));
                 })
             );
         } else {
             return timer(200).pipe(
                 switchMap(() => {
-                    if (control.pristine || control.value === null) { return of(null); }
-                    return this.httpClient.post(`${this.urls.apiUrl}/gameservers/${control.value}`, {}).pipe(
-                        map(response => (response ? { serverTaken: true } : null))
-                    );
+                    if (control.pristine || control.value === null) {
+                        return of(null);
+                    }
+                    return this.httpClient.post(`${this.urls.apiUrl}/gameservers/${control.value}`, {}).pipe(map((response) => (response ? { serverTaken: true } : null)));
                 })
             );
         }
@@ -121,25 +121,35 @@ export class AddServerModalComponent {
             this.server.adminPassword = this.form.controls['adminPassword'].value;
             this.server.environment = this.form.controls['environment'].value;
             this.server.serverOption = this.form.controls['serverOption'].value;
-            this.httpClient.patch(`${this.urls.apiUrl}/gameservers`, this.server, {
-                headers: new HttpHeaders({
-                    'Content-Type': 'application/json'
+            this.httpClient
+                .patch(`${this.urls.apiUrl}/gameservers`, this.server, {
+                    headers: new HttpHeaders({
+                        'Content-Type': 'application/json',
+                    }),
                 })
-            }).subscribe((environmentChanged: boolean) => {
-                this.dialogRef.close(environmentChanged);
-            }, _ => {
-                this.submitting = false;
-            });
+                .subscribe(
+                    (environmentChanged: boolean) => {
+                        this.dialogRef.close(environmentChanged);
+                    },
+                    (_) => {
+                        this.submitting = false;
+                    }
+                );
         } else {
-            this.httpClient.put(`${this.urls.apiUrl}/gameservers`, JSON.stringify(this.form.getRawValue()), {
-                headers: new HttpHeaders({
-                    'Content-Type': 'application/json'
+            this.httpClient
+                .put(`${this.urls.apiUrl}/gameservers`, JSON.stringify(this.form.getRawValue()), {
+                    headers: new HttpHeaders({
+                        'Content-Type': 'application/json',
+                    }),
                 })
-            }).subscribe(_ => {
-                this.dialogRef.close(false);
-            }, _ => {
-                this.submitting = false;
-            });
+                .subscribe(
+                    (_) => {
+                        this.dialogRef.close(false);
+                    },
+                    (_) => {
+                        this.submitting = false;
+                    }
+                );
         }
     }
 }
