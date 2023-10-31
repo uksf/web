b715bcaa5c1bbb8b9a96565d6089050549e2ba60
Tim Beswick <contact.tim.here@gmail.com>
2023-10-31 17:44:13 +0000

    Handle copyright better on mobile page

--- src/app/Components/footer-bar/footer-bar.component.ts
+++ src/app/Components/footer-bar/footer-bar.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-footer-bar',
     templateUrl: './footer-bar.component.html',
     styleUrls: ['./footer-bar.component.scss']
 })
-export class FooterBarComponent {
-    constructor() { }
+export class FooterBarComponent implements OnInit {
+    mobile = false;
+    mobileSmall = false;
+
+    constructor() {}
+
+    ngOnInit() {
+        this.mobile = window.screen.width <= 768 && window.screen.width > 375;
+        this.mobileSmall = window.screen.width <= 375;
+    }
+
+    @HostListener('window:resize')
+    onResize() {
+        this.mobile = window.screen.width <= 768 && window.screen.width > 375;
+        this.mobileSmall = window.screen.width <= 375;
+    }
 }
